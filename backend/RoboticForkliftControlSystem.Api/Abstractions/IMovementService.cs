09989c298d3a2f277123e62082186ce44fe730b1
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2025-08-16 19:16:04 +0700

    removed all necessary imported

--- backend/RoboticForkliftControlSystem.Api/Abstractions/IMovementService.cs
+++ backend/RoboticForkliftControlSystem.Api/Abstractions/IMovementService.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using RoboticForkliftControlSystem.Api.Entities;
 
 namespace RoboticForkliftControlSystem.Api.Abstractions;
--- backend/RoboticForkliftControlSystem.Api/Services/MovementService.cs
+++ backend/RoboticForkliftControlSystem.Api/Services/MovementService.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using RoboticForkliftControlSystem.Api.Abstractions;
 using RoboticForkliftControlSystem.Api.Entities;
 
