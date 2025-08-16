783a5848d9aeb2a030e1bbacca1325b61029575a
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2025-08-16 18:49:50 +0700

    inital migration included

---
+++ backend/RoboticForkliftControlSystem.Api/Migrations/20250816114212_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoboticForkliftControlSystem.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forklifts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ModelNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ManufacturingDate = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forklifts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Forklifts");
+        }
+    }
+}
